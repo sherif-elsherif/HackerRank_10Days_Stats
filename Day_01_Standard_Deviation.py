1a51a51c64573012f6a519bea3ab276554341bc9
Sherif ElSherif <55397885+sherif-elsherif@users.noreply.github.com>
2020-03-25 11:23:39 -0400

    Update Day_01_Standard_Deviation.py

--- Day_01_Standard_Deviation.py
+++ Day_01_Standard_Deviation.py
@@ -3,21 +3,26 @@ import math as m
 # getting input from user
 n = int(input())
 arr = input().split(' ')
-weight = input().split(' ')
 
 i = 0
 while i < n:
     arr[i] = int(arr[i])
-    weight[i] = int(weight[i])
     i += 1
 
 # calculating the mean
 sum_values = 0
-sum_weights = 0
 i = 0
 while i < n:
-    sum_values += arr[i] * weight[i]
-    sum_weights += weight[i]
+    sum_values += arr[i]
     i += 1
-mean = sum_values/sum_weights
-print(round(mean, 1))
+mean = sum_values/n
+
+# calculate the Standard Deviation
+sum = 0
+i = 0
+while i<n:
+  sum += (arr[i]-mean)**2
+  i += 1
+
+sd = m.sqrt(sum/n)
+print(round(sd, 1))
